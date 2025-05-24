a51592540917bf9152cfcee89da40b5dc15cded9
martvanrijthoven <mart.vanrijthoven@gmail.com>
2025-05-25 01:02:06 +0200

    update script

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 # Run pytest with coverage and show a report
-export TEST_DB_USER="yamlo_user"
-export MY_TEST_VAR="yamlo_env"
-export TEST_DB_PASS="yamlo_pass"
 coverage run -m pytest -s "$@"
 coverage report -m
 coverage html
