751ea1700b4c5a053bb4346226a87dc6c67fc233
martvanrijthoven <mart.vanrijthoven@gmail.com>
2025-05-25 01:17:18 +0200

    remove file

--- run_coverage.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Run pytest with coverage and show a report
-coverage run -m pytest -s "$@"
-coverage report -m
-coverage html
-
-echo "\nHTML coverage report generated at htmlcov/index.html"
