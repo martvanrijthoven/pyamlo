68dfeed4a15cbe7c5811065edccd7f559ea6257c
martvanrijthoven <mart.vanrijthoven@gmail.com>
2025-05-26 12:42:07 +0200

    update pyproject

--- pyamlo/__init__.py
+++ pyamlo/__init__.py
@@ -6,11 +6,3 @@ from .config import SystemInfo, load_config
 from .resolve import call
 
 __all__ = ["load_config", "SystemInfo", "call"]
-
-
-class Step:
-    def __init__(self, name: str, inputs: dict = None):
-        self.name = name
-        self.inputs = inputs
-        self.outputs = None
-
